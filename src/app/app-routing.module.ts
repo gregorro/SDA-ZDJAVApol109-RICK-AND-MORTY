c0a2d04af00d5289e7e25ca6374ccd624ae42099
Greg <39492220+gregorro@users.noreply.github.com>
2023-05-11 18:23:02 +0200

    feat: create basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharactersTableComponent } from './components/characters-table/characters-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'characters', component: CharactersTableComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharactersTableComponent } from './components/characters-table/characters-table.component';
 import { CharactersTableRowComponent } from './components/characters-table-row/characters-table-row.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CharactersTableComponent,
-    CharactersTableRowComponent
+    CharactersTableRowComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
