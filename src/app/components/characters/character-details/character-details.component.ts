c43346c70a6a5b6938476ad759d79a2869142678
Greg <39492220+gregorro@users.noreply.github.com>
2023-05-11 20:39:11 +0200

    feat: todo v2

--- src/app/components/characters/character-details/character-details.component.ts
+++ src/app/components/characters/character-details/character-details.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Route, Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-character-details',
@@ -12,4 +12,10 @@ export class CharacterDetailsComponent {
   constructor(private router: ActivatedRoute){
     this.characterId = this.router.snapshot.params['id']
   }
+
+  // TODO - zadanko
+  // 1 - stworzyc metody w serwisie do pobierania konkretnego charakteru
+  // 2 - wyswietlić dane zwrocone z api (stworzyc html)
+  // 3 - zrobic aktywna nawigacje do kazdego z innych miejsc
+  // 3* - przeniesc metode getIdByLink z componentu do zewnetrzenego pliku util
 }
